84c933d09108833a56aa5973720afc12befe903d
c60cb859 <christensen.theis@gmail.com>
2018-07-31 11:48:42 +0200

    added ecb mode detection

--- crypto_tools/aes_ecb.py
+++ crypto_tools/aes_ecb.py
@@ -15,3 +15,35 @@ def dec_aes_ecb(key, cipher):
     cleartext = algrorithm.decrypt(cipher)
 
     return cleartext
+
+
+def split_cipher(block_size, cipher):
+    data = list()
+    for num in range(0, len(cipher), block_size):
+        data.append(cipher[num:num+block_size])
+
+    return data
+
+
+def fint_duplicates(data):
+    seen = dict()
+    duplicates = list()
+
+    for block in data:
+        if block not in seen:
+            seen[block] = 1
+        else:
+            if seen[block] == 1:
+                duplicates.append(block)
+            seen[block] += 1
+
+    return duplicates
+
+
+def detect_ecb_mode(block_size, cipher):
+    blocks = split_cipher(block_size, cipher)
+    duplicates = fint_duplicates(blocks)
+
+    if len(duplicates) > 0:
+        return True
+    return False
--- tests/test_01_basics.py
+++ tests/test_01_basics.py
@@ -98,6 +98,23 @@ class CryptoChallengeSet1(unittest.TestCase):
 
         self.assertEqual(result, cleartext)
 
+    def test_detect_aes_in_ecb_mode(self):
+        result = 'd880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf' +\
+                 '9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a' +\
+                 '08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4f' +\
+                 'd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a'
+        block_size = 16
+
+        with open('files/8.txt') as f:
+            data = f.read().split('\n')[:-1]
+
+        for cipher in data:
+            if aes.detect_ecb_mode(block_size, cipher):
+                ecb_cipher = cipher
+                break
+
+        self.assertEqual(result, ecb_cipher)
+
 
 if __name__ == '__main__':
     unittest.main()

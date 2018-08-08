46987eb2cd67edfc6f0213231892d0cc30252e90
Theis Christensen <tcr@napatech.com>
2018-08-08 11:33:28 +0200

    CBC encrypt/decrypt
    I made a working CBC encrypt and decrypt function,
    using the ECB encrypt and decrypt functions,
    together with fixed XOR

--- cbc_mode.py
+++ cbc_mode.py
@@ -5,30 +5,67 @@ import crypto_tools.data_conversion as dc
 import crypto_tools.byte_operations as bo
 
 
-block_size = 16
-IV = dc.utf8_to_bytes('0' * block_size)
+def enc_aes_cbc(cleartext, key, iv=0, block_size=16):
+    if iv == 0:
+        iv = bytes(block_size)
+    prev_block = iv
+
+    padded_cleartext = aes.padding_pkcs7(block_size, cleartext)
+    data = aes.split_cipher(block_size, padded_cleartext)
+
+    byte_cipher = list()
+
+    for block in data:
+        xored_block = bo.fixed_xor(prev_block, block)
+        byte_cipher.append(aes.enc_aes_ecb(key, bytes(xored_block)))
+        prev_block = byte_cipher[-1]
+
+    cipher = bytes()
+
+    for block in byte_cipher:
+        cipher += block
+
+    return cipher
+
+
+def dec_aes_cbc(ciphertext, key, iv=0, block_size=16):
+    if iv == 0:
+        iv = bytes(block_size)
+    prev_block = iv
+
+    data = aes.split_cipher(block_size, ciphertext)
+
+    byte_cleartext = list()
+
+    for block in data:
+        xored_block = aes.dec_aes_ecb(key, block)
+        byte_cleartext.append(bo.fixed_xor(prev_block, xored_block))
+        prev_block = block
+
+    cleartext = bytes()
+
+    for block in byte_cleartext:
+        cleartext += block
+
+    return cleartext
+
+
 key = 'YELLOW SUBMARINE'
 
-cleartext = 'CBC mode is a block cipher mode that allows us to encrypt irregularly-sized messages,' +\
-            ' despite the fact that a block cipher natively only transforms individual blocks'
+cleartext = 'CBC mode is a block cipher mode that allows us to encrypt irregularly-sized messages, despite the fact that a block cipher natively only transforms individual blocks.'
+byte_cleartext = dc.utf8_to_bytes(cleartext)
 
-print(len(cleartext) % 16)
-padded_cleartext = aes.padding_pkcs7(block_size, cleartext)
-print(len(padded_cleartext)/block_size)
-data = aes.split_cipher(block_size, padded_cleartext)
+dec_cleartext = dec_aes_cbc(enc_aes_cbc(byte_cleartext, key), key)
 
-byte_data = list()
-for block in data:
-    byte_data.append(dc.utf8_to_bytes(block))
 
-byte_cipher = list()
-print(len(byte_data[-1]))
+print('-'*10 + 'TESTING' + '-'*10)
+print(dec_cleartext)
+print('-'*10 + 'TESTING' + '-'*10)
 
-for num in range(0, len(byte_data)):
-    if num == 0:
-        xored_block = bo.fixed_xor(IV, byte_data[num])
-    else:
-        xored_block = bo.fixed_xor(byte_cipher[num-1], byte_data[num])
-    byte_cipher.append(aes.enc_aes_ecb(key, bytes(xored_block)))
+with open('files/10.txt') as f:
+    base64_cipher_text = f.read().replace('\n', '')
 
-print(byte_cipher)
+cipher = dc.base64_to_bytes(base64_cipher_text)
+dec_cleartext = dec_aes_cbc(cipher, key)
+
+print(dc.bytes_to_utf8(dec_cleartext))

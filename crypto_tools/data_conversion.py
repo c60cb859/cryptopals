38f77fac7f50b78e3dc0a3bc450c354e2927a19a
c60cb859 <christensen.theis@gmail.com>
2018-08-11 12:10:01 +0200

    Docstring update
    I have updated the docstring and tests for data converters

--- crypto_tools/data_conversion.py
+++ crypto_tools/data_conversion.py
@@ -1,4 +1,9 @@
-#!/bin/python3
+#!/usr/bin/env python3
+'''This files contains converter classes for converting some form a data into bytearrays,
+these classes are used in combination with the ByteData class when initializing and encoding data.
+
+A converter class shall follow the DataConverter interface.
+'''
 
 import base64
 import codecs
@@ -7,16 +12,21 @@ from math import floor, ceil, log2
 
 
 class DataConverter:
+    '''This is used as interface for DataConverter classes
+
+    A DataConverter class needs a encoder and decoder methode
+    '''
     def encode(self, data):
         '''Encode take a bytearray and convert it to another type, and returns it'''
         pass
 
     def decode(self, data):
-        '''Decode takes data and convert it to a bytearray, and returns it'''
+        '''Decode takes some data and convert it to a bytearray, and returns it'''
         pass
 
 
 class HexConverter(DataConverter):
+    '''Converts Hex data'''
     def encode(self, data):
         hex_string = data.hex()
         return hex_string
@@ -27,6 +37,7 @@ class HexConverter(DataConverter):
 
 
 class Base64Converter(DataConverter):
+    '''Converts Base64 data'''
     def encode(self, data):
         base64_string = codecs.encode(data, 'base64')
         utf8_printable = base64_string.decode('utf-8')[:-1]
@@ -38,6 +49,7 @@ class Base64Converter(DataConverter):
 
 
 class UTF8Converter(DataConverter):
+    '''Converts UTF8 data'''
     def encode(self, data):
         utf8_string = data.decode('latin-1')
         return utf8_string
@@ -48,6 +60,7 @@ class UTF8Converter(DataConverter):
 
 
 class IntConverter(DataConverter):
+    '''Converts integer'''
     def encode(self, data):
         return int.from_bytes(data, byteorder='big')
 
--- tests/test_data_conversion.py
+++ tests/test_data_conversion.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+'''This files contains unittests for the DataConverter classes'''
 
 import unittest
 
@@ -8,8 +9,10 @@ from crypto_tools.data_conversion import UTF8Converter
 from crypto_tools.data_conversion import IntConverter
 
 
-class DataConversion(unittest.TestCase):
-    def test_hex_to_bytes(self):
+class TestHexConverter(unittest.TestCase):
+    '''Test classe for HexConverter'''
+    def test_hex_decode(self):
+        '''Make sure decoding hex works'''
         result = bytes([0x1b, 0x37, 0x37, 0x33, 0x31, 0x36, 0x3f, 0x78, 0x18])
         hex_string = '1b37373331363f7818'
 
@@ -17,31 +20,8 @@ class DataConversion(unittest.TestCase):
 
         self.assertEqual(result, byte)
 
-    def test_base64_to_bytes(self):
-        result = b'Man is distinguished, not only by his reason, but by this'
-        base64_string = 'TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz'
-
-        byte = Base64Converter().decode(base64_string)
-
-        self.assertEqual(result, byte)
-
-    def test_utf8_to_bytes(self):
-        result = b'Man is distinguished, not only by his reason, but by this'
-        utf8_string = 'Man is distinguished, not only by his reason, but by this'
-
-        byte = UTF8Converter().decode(utf8_string)
-
-        self.assertEqual(result, byte)
-
-    def test_int_to_single_byte(self):
-        result = bytes([0xc9])
-        integer = 201
-
-        byte = IntConverter().decode(integer)
-
-        self.assertEqual(result, byte)
-
-    def test_bytes_to_hex(self):
+    def test_hex_encode(self):
+        '''Make sure hex encoding works'''
         result = '1b37373331363f7818'
         byte = bytes([0x1b, 0x37, 0x37, 0x33, 0x31, 0x36, 0x3f, 0x78, 0x18])
 
@@ -49,7 +29,20 @@ class DataConversion(unittest.TestCase):
 
         self.assertEqual(result, hex_string)
 
-    def test_bytes_to_base64(self):
+
+class TestBase64Converter(unittest.TestCase):
+    '''Test classe for Base64Converter'''
+    def test_base64_decode(self):
+        '''Make sure decoding base64 works'''
+        result = b'Man is distinguished, not only by his reason, but by this'
+        base64_string = 'TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz'
+
+        byte = Base64Converter().decode(base64_string)
+
+        self.assertEqual(result, byte)
+
+    def test_base64_encode(self):
+        '''Make sure base64 encoding works'''
         result = 'TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz'
         byte = b'Man is distinguished, not only by his reason, but by this'
 
@@ -57,13 +50,47 @@ class DataConversion(unittest.TestCase):
 
         self.assertEqual(result, base64_string)
 
-    def test_bytes_to_utf8(self):
+
+class TestUTF8Converter(unittest.TestCase):
+    '''Test classe for UTF8Converter'''
+    def test_utf8_decode(self):
+        '''Make sure decoding UTF8 works'''
+        result = b'Man is distinguished, not only by his reason, but by this'
+        utf8_string = 'Man is distinguished, not only by his reason, but by this'
+
+        byte = UTF8Converter().decode(utf8_string)
+
+        self.assertEqual(result, byte)
+
+    def test_utf8_encode(self):
+        '''Make sure UTF8 encoding works'''
         result = 'Man is distinguished, not only by his reason, but by this'
         byte = b'Man is distinguished, not only by his reason, but by this'
 
         utf8_string = UTF8Converter().encode(byte)
 
         self.assertEqual(result, utf8_string)
+
+
+class TestIntConverter(unittest.TestCase):
+    '''Test classe for IntConverter'''
+    def test_int_decode(self):
+        '''Make sure decoding intergers works'''
+        result = bytes([0x02, 0xc1, 0xd1])
+        integer = 180689
+
+        byte = IntConverter().decode(integer)
+
+        self.assertEqual(result, byte)
+
+    def test_int_encode(self):
+        '''Make sure integer encoding works'''
+        result = 180689
+        byte = bytes([0x02, 0xc1, 0xd1])
+
+        integer = IntConverter().encode(byte)
+
+        self.assertEqual(result, integer)
 
 
 if __name__ == '__main__':
